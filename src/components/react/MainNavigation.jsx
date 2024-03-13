61bbdafa88b4c2cc182bd60fc04574e8f7236247
Gilberto Hernandez Amura <snantdev@gmail.com>
2024-03-13 16:14:54 -0300

    Se añadió un BaseLayout y una navegación responsiva

---
+++ src/components/react/MainNavigation.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import {
+    Navbar,
+    NavbarBrand,
+    NavbarContent,
+    NavbarItem,
+    NavbarMenuToggle,
+    NavbarMenu,
+    NavbarMenuItem,
+    Link,
+    Image
+} from "@nextui-org/react";
+import LogoPlaceHolder from "../../../public/LogoPlaceHolder.svg";
+
+const menuItems = [
+    { text: "Inicio", link: "/" },
+    { text: "Academia", link: "/academy/" },
+    { text: "Maestros", link: "/teachers/" },
+    { text: "Precios", link: "/#pricing" },
+]
+
+export const MainNavigation = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState("false");
+
+    return (
+        <Navbar onMenuOpenChange={setIsMenuOpen}>
+            <NavbarContent>
+                <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} className="sm:hidden" />
+                <NavbarBrand >
+                    <Image src={LogoPlaceHolder.src} />
+                </NavbarBrand>
+            </NavbarContent>
+
+            <NavbarContent className="hidden sm:flex gap-8" justify="end" >
+                <NavbarItem>
+                    <Link color="foreground" underline="hover" href="/">
+                        Inicio
+                    </Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Link color="foreground" underline="hover" href="/academy/">
+                        Academia
+                    </Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Link color="foreground" underline="hover" href="/teachers/">
+                        Maestros
+                    </Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Link color="foreground" underline="hover" href="/#pricing">
+                        Precios
+                    </Link>
+                </NavbarItem>
+            </NavbarContent>
+
+            <NavbarMenu>
+                {menuItems.map((menuItem, idx) => (
+                    <NavbarMenuItem key={`${menuItem.text}-${idx}`}>
+                        <Link color="foreground" size="lg" className="w-full" href={menuItem.link} >{menuItem.text}</Link>
+                    </NavbarMenuItem>)
+                )}
+            </NavbarMenu>
+        </Navbar>
+    )
+}
