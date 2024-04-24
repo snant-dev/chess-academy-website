a358f428aa0013d03aa5f9de36e17e40f6e12911
Gilberto Hernandez Amura <snantdev@gmail.com>
2024-04-24 20:49:34 -0300

    Se añadió el Camino de las Damas y sesoluciono la responsibidad de las tabs

--- src/components/react/PathsTabs.jsx
+++ src/components/react/PathsTabs.jsx
@@ -13,7 +13,7 @@ export const PathsTabs = () => {
         size="lg"
         classNames={{
           tabList:
-            "gap-2 sm:gap-12 w-full relative rounded-none p-0  border-divider",
+            "sm:gap-8 md:gap-12 w-full relative rounded-none p-0  border-divider flex flex-col sm:flex-row",
           cursor: "w-full bg-[#ff8c00]",
           tab: "max-w-fit px-0 h-12 ",
           tabContent: "group-data-[selected=true]:text-[#ff8c00]",
@@ -87,6 +87,40 @@ export const PathsTabs = () => {
               ¡Descubre el camino "Ajedrez de Competición" y prepárate para
               desafiar tus límites y alcanzar nuevos logros en el apasionante
               mundo del ajedrez competitivo!
+            </p>
+          </div>
+        </Tab>
+        <Tab
+          key="damas"
+          title={
+            <div className="flex items-center space-x-2">
+              <span>El Camino de las Damas</span>
+            </div>
+          }
+        >
+          <div>
+            <h3 className="mb-8 mt-4 text-2xl tracking-tight font-extrabold text-gray-900 dark:text-white text-center">
+              El Camino de las Damas
+            </h3>
+            <p className="mb-4 leading-loose text-justify">
+              En nuestra nueva propuesta nos enfocamos en crear un espacio único
+              y agradable para las damas en el ajedrez. Reconocemos el esfuerzo
+              y la dedicación de las mujeres en un deporte mayoritariamente
+              practicado por hombres, y queremos promover su participación
+              activa en todos los niveles del ajedrez.
+            </p>
+            <p className="mb-4 leading-loose text-justify">
+              Este Camino es transversal a los demás. Queremos destacar el
+              talento y la capacidad de las mujeres en el ajedrez, fomentando su
+              participación como ajedrecistas, entrenadoras, árbitros y líderes
+              en la comunidad ajedrecística.
+            </p>
+            <p className="leading-loose text-justify">
+              Si estás buscando un espacio donde puedas disfrutar, entrenarte y
+              crecer en un ambiente especialmente diseñado para mujeres y niñas,
+              este camino es para ti. Te invitamos a seguirlo con el objetivo de
+              superarte cada día y alcanzar tus metas, incluyendo la conquista
+              de títulos absolutos en el mundo del ajedrez.
             </p>
           </div>
         </Tab>
